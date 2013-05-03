f873c181c00bef68fef1efb123b8c5ab5ee68b9c
Alvin Yan <yan.hongyao.alvin@dhs.sg>
2013-05-03 23:56:50 +0800

    Commit v2. Many things.

--- flask/createdata.py
+++ flask/createdata.py
@@ -1,25 +1,14 @@
 import sqlite3 as lite
 import sys
-item = (
-	('A4 lecture pad', 2.60),
-	('7-colour sticky note with pen', 4.20),
-	('A5 ring book', 4.80),
-	('A5 note book with zip bag', 4.60),
-	('2B pencil', 0.90),
-	('Stainless steel tumbler', 12.90),
-	('A4 clear holder', 4.40),
-	('A4 vanguard file', 1.00),
-	('Name card holder', 10.90),
-	('Umbrella', 9.00),
-	('School badge (Junior High)', 1.30),
-	('School badge (Senior High)', 1.80),
-	('Dunman dolls (pair)', 45.00)	
-)
 
 con = lite.connect('shop.db')
 
 with con:
 	cur = con.cursor()
-	
-	cur.execute("CREATE TABLE person (name text, lecturepad text")
-	cur.executemany("INSERT INTO items VALUES (?, ?)", item)
+	#cur.execute("DROP TABLE IF EXISTS orders")
+	#cur.execute("DROP TABLE IF EXISTS users")
+	#cur.execute("CREATE TABLE users(username text, password text, email text)")
+	#cur.execute("INSERT INTO users VALUES ('admin', 'admin', 'leow.justin@dhs.sg')")
+	#cur.execute("INSERT INTO users VALUES ('Aussiroth', 'WhiteDragon', 'yan.hongyao.alvin@dhs.sg')")
+	#cur.execute("CREATE TABLE orders (name text, lecture number, stickynote number, exercisebook number, notebook number, pencil number, tumbler number, clearholder number, vanguard number, cardholder number, umbrella number, jhbadge number, shbadge number, dolls number)")
+	#cur.execute("INSERT INTO orders VALUES ('admin', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)")
--- flask/routes.py
+++ flask/routes.py
@@ -4,7 +4,7 @@ import sqlite3
 
 app = Flask(__name__)
 
-app.secret_key = ('Aussircaex')
+app.secret_key = ('GreatWhiteBlueRedBlackGreenDragonsAreSoMuchCoolerThanTheStupidHouseDrakon')
 
 DATABASE = 'shop.db'
 
@@ -19,7 +19,7 @@ def login_required(test):
         if 'logged_in' in session:
             return test(*args, **kwargs)
         else:
-            flash('You need to login first.')
+            flash('Please login to access the site.')
             return redirect(url_for('login'))
     return wrap 
 
@@ -32,14 +32,7 @@ def check_logged_in():
 @app.route('/')
 @login_required
 def home():
-    #g.db  = connect_db()
-    #cur = g.db.execute('select name, cost from items')
-    #sales = [dict(name=row[0], cost=row[1]) for row in cur.fetchall()]
-    #g.db.close()
-    if check_logged_in():
-		return render_template('home.html', logged = True)
-    else:
-		return render_template('home.html', logged = False)
+	return render_template('home.html', logged = True)
 
 @app.route('/logout')
 def logout():
@@ -57,31 +50,128 @@ def welcome():
 @app.route('/cart')
 @login_required
 def cart():
-	if check_logged_in():
-		return render_template('cart.html', logged = True)
-	else:
-		return render_template('cart.html', logged = False)
+	g.db = connect_db()
+	cur = g.db.execute('select name, lecture, stickynote, exercisebook, notebook, pencil, tumbler, clearholder, vanguard, cardholder, umbrella, jhbadge, shbadge, dolls from orders')
+	orders = [[row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13]] for row in cur.fetchall()]
+	#check for persons order
+	for order in orders:
+		if row[0] == logged_user:
+			currorder = order
+			break
+	return render_template('cart.html', currorder = currorder, logged = True)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            session['logged_in'] = True
-            return redirect(url_for('welcome'))
+	error = None
+	debug = 0
+	if request.method == 'POST':
+		g.db = connect_db()
+		cur = g.db.execute('select username, password from users')
+		users = [[row[0], row[1]] for row in cur.fetchall()]
+		found = False
+		curruser = request.form['username']
+		currpass = request.form['password']
+		for user in users:
+			if user[0]==curruser:
+				found = True
+				if user[1]==currpass:
+					session['logged_in'] = True
+					global logged_user
+					logged_user = curruser
+					return redirect(url_for('welcome'))
+				else:
+					error = "You entered the wrong password."
+		if not found:
+			error = "You entered a wrong username."
 	if check_logged_in():
 		return render_template('log.html', error=error, logged = True)
-    else:
+	else:
 		return render_template('log.html', error=error, logged = False)
 
+@app.route('/signup', methods = ['GET', 'POST'])
+def signup():
+	error = []
+	if request.method == 'POST':
+		curruser = request.form['username']
+		password1 = request.form['password1']
+		password2 = request.form['password2']
+		curremail = request.form['email']
+		correct = True
+		# VALIDATE EMAIL
+		search = curremail.find("@")
+		if search<0:
+			error.append("You did not enter your email correctly, there must be a @ symbol in the email address, e.g. lim@def.com")
+			curremail = ""
+			correct = False
+		elif search==0:
+			error.append("You did not enter your email correctly, there should be some text before the @ symbol, e.g. ah@def.com")
+			curremail = ""
+			correct = False
+		else:
+			if "." not in curremail:
+				error.append("You did not enter your email correctly, there should be a . after the @ symbol, e.g. seng@def.com")
+				curremail = ""
+				correct = False
+		#VALIDATE IF BOTH PASSWORDS ENTERED MATCH
+		if password1 != password2:
+			error.append("The passwords you entered are not the same! Please reenter.")
+			password1 = ""
+			correct = False
+			
+		#VALIDATE IF USERNAME ALREADY USED
+		
+		g.db = connect_db()
+		cur = g.db.execute('select username from users')
+		users = [row[0] for row in cur.fetchall()]
+		if curruser in users:
+			error.append("The username is already taken. Please choose another one.")
+			curruser = ""
+			correct = False
+		
+		# IF ALL DATA CORRECT, ENTER INTO DATABASE, REDIRECT TO WELCOME
+			
+		if correct==True:
+			session['logged_in'] = True
+			g.db.execute('insert into users (username, password, email) values (?, ?, ?)', [curruser, password1, curremail])
+			g.db.commit()
+			global logged_user
+			logged_user = curruser
+			flash('Thanks for signing up!')
+			return redirect(url_for('welcome'))
+		else:
+			return render_template('signup.html', username = curruser, password = password1, email = curremail, error = error, logged = False)
+	else:
+		return render_template('signup.html', logged = False)
+		
+#MIDDLE-PAGE FOR STORING ORDERS, THEN LINK STRAIGHT TO CART.
+		
 @app.route('/order', methods=['GET', 'POST'])
 @login_required
 def order():
-	item = request.form['item']
-	number = request.form['value']
-	return render_template('order.html', number = number)
+	number=[]
+	number.append(int(request.form['item1']))
+	number.append(int(request.form['item2']))
+	number.append(int(request.form['item3']))
+	number.append(int(request.form['item4']))
+	number.append(int(request.form['item5']))
+	number.append(int(request.form['item6']))
+	number.append(int(request.form['item7']))
+	number.append(int(request.form['item8']))
+	number.append(int(request.form['item9']))
+	number.append(int(request.form['item10']))
+	number.append(int(request.form['item11']))
+	number.append(int(request.form['item12']))
+	number.append(int(request.form['item13']))
+	g.db = connect_db()
+	cur = g.db.execute('select name from orders')
+	names = [row[0] for row in cur.fetchall()]
+	if logged_user in names:
+		g.db.execute('delete from orders where name='+str(logged_user))
+		#g.db.execute('update orders set (lecture, stickynote, exercisebook, notebook, pencil, tumbler, clearholder, vanguard, cardholder, umbrella, jhbadge, shbadge, dolls) = (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) where name='+str(logged_user), [number])
+	g.db.execute('insert into orders (name, lecture, stickynote, exercisebook, notebook, pencil, tumbler, clearholder, vanguard, cardholder, umbrella, jhbadge, shbadge, dolls) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [logged_user, number[0], number[1], number[2], number[3], number[4], number[5], number[6], number[7], number[8], number[9], number[10], number[11], number[12]])
+	g.db.commit()
+	return redirect(url_for('cart'))
+	#return render_template('order.html', loggeduser = logged_user, names = number)
 		
 if __name__ == '__main__':
     app.run(debug=True)
